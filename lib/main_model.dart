3c29064a59450ce94c0e379e86b8c86c1baff95d
BBee <atsushi.kokubun.47@gmail.com>
2023-05-25 06:42:41 +0900

    Port buisiness logic to model

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// ViewとModelの橋渡しとなるProvider
+final mainProvider = ChangeNotifierProvider((_) => MainModel());
 
 class MainModel extends ChangeNotifier {
-  int a = 0;
-  void change() {
-    a = 2;
-    notifyListeners(); // 通知を受け取ったウィジェットを再描画する
+  int counter = 0; // 変数の先頭に_はprivateなので、つけないようにする
+
+  void incrementCounter() {
+    counter++;
+    // setState()はStatefulWidgetでのみ使えるのでここでは使えない
+    // 代わりにnotifyListeners()を使う
+    notifyListeners(); // これを呼ぶと、変更を検知して再描画してくれる
   }
 }
