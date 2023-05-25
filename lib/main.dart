f546516a9a226eadd4d21b4e8657a67da203972d
BBee <atsushi.kokubun.47@gmail.com>
2023-05-25 13:37:05 +0900

    Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+// flutter
 import 'package:flutter/material.dart';
-import 'package:flutter_application_test/main_model.dart';
+// package
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+// models
+import 'package:flutter_application_test/main_model.dart';
+// options
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
