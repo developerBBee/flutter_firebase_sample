59f4fc6ea8cab79ebf39438526d6287e92ca478d
BBee <atsushi.kokubun.47@gmail.com>
2023-05-25 18:34:56 +0900

    Use cloud firestore

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,4 +1,8 @@
+// flutter
 import 'package:flutter/material.dart';
+
+// packages
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // ViewとModelの橋渡しとなるProvider
@@ -7,8 +11,20 @@ final mainProvider = ChangeNotifierProvider((_) => MainModel());
 class MainModel extends ChangeNotifier {
   int counter = 0; // 変数の先頭に_はprivateなので、つけないようにする
 
-  void incrementCounter() {
+  Future<void> createUser({required BuildContext context}) async {
     counter++;
+    // Firestoreに保存
+    final Map<String, dynamic> userData = {
+      "userName": "Alice",
+      "uid": "firstUser",
+    };
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc('firstUser')
+        .set(userData);
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('ユーザーが作成できました')));
+
     // setState()はStatefulWidgetでのみ使えるのでここでは使えない
     // 代わりにnotifyListeners()を使う
     notifyListeners(); // これを呼ぶと、変更を検知して再描画してくれる
