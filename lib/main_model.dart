f4fee4410006af97a35046f778a8bf2dcc736047
BBee <atsushi.kokubun.47@gmail.com>
2023-05-25 19:35:03 +0900

    Add dependencies

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 // packages
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
 
 // ViewとModelの橋渡しとなるProvider
 final mainProvider = ChangeNotifierProvider((_) => MainModel());
@@ -13,14 +14,16 @@ class MainModel extends ChangeNotifier {
 
   Future<void> createUser({required BuildContext context}) async {
     counter++;
+    final uuid = Uuid();
+    final String v4 = uuid.v4();
     // Firestoreに保存
     final Map<String, dynamic> userData = {
       "userName": "Alice",
-      "uid": "firstUser",
+      "uid": v4,
     };
     await FirebaseFirestore.instance
         .collection('users')
-        .doc('firstUser') // ここでドキュメントIDを指定(指定しない場合は自動でIDが振られる)
+        .doc(v4) // ここでドキュメントIDを指定(指定しない場合は自動でIDが振られる)
         .set(userData);
     ScaffoldMessenger.of(context)
         .showSnackBar(SnackBar(content: Text('ユーザーが作成できました')));
